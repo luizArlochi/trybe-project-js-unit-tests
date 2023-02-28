0fd02b12649915b16f14fd8e2869e7d2d38daf71
Luiz Arlochi <luiz.arlochi@gmail.com>
2023-02-28 18:42:33 -0300

    requisito 4 concluido

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -22,15 +22,62 @@ const circle = require('../src/circle');
 */
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
-  it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado possui 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
-    // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
+  it('Testa se a função circle retorna undefined quando o parâmetro passado não é um número', () => {
+    expect(circle('string')).toBeUndefined();
+  });
+
+  it('Testa se a função circle retorna um objeto', () => {
+    const validCircle = circle(5);
+    expect(typeof validCircle).toBe('object');
+  });
+
+  it('Testa se o objeto retornado pela função circle possui 3 propriedades', () => {
+    const validCircle = circle(5);
+    expect(Object.keys(validCircle)).toHaveLength(3);
+  });
+
+  it('Testa se a função circle retorna undefined quando não recebe nenhum parâmetro', () => {
+    const invalidCircle = circle();
+    expect(invalidCircle).toBeUndefined();
+  });
+
+  it('Testa se o objeto retornado pela função circle possui a circunferência correta para um círculo de raio 2', () => {
+    const validCircle = circle(2);
+    expect(validCircle.circumference).toBe(12.56);
+  });
+
+  it('Testa se o objeto retornado pela função circle possui a área correta para um círculo de raio 3', () => {
+    const validCircle = circle(3);
+    expect(validCircle.circumference).toBe(18.84);
+  });
+
+  it('Testa se a função circle retorna os dados corretos de um círculo de raio 3', () => {
+    const validCircle = circle(3);
+    expect(validCircle).toEqual({
+      radius: 3,
+      area: 28.259999999999998,
+      circumference: 18.84,
+    });
   });
 });
+
+// Tentei colocar os parametros do que é esperado de forma a não setar um valor especifico,
+// e sim com que o teste fizesse o calculo utilizando os parametros da função, por exemplo
+
+// it('Testa se a função circle retorna os dados corretos de um círculo de raio 3', () => {
+//   const validCircle = circle(3);
+//   expect(validCircle).toBeClose({
+//     radius: 3,
+//     area: Math.PI * 3 * 3,
+//     circumference: 2 * Math.PI * 3,
+//   });
+// });
+
+// O que acredito eu teria uma lógica mais próxima do ideal, 
+// porém não estou sabendo usar os matchers e o teste está retornando um erro referente as casas decimais
+// Expected
+//     -   "area": 28.274333882308138,
+// -   "circumference": 18.84955592153876,
+// Received
+// +   "area": 28.259999999999998,
+// +   "circumference": 18.84,
